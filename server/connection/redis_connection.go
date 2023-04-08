a88d035d31209b3c3dc915b5b5202dc66acc07a6
Neo Jarmawijaya <neojarmawijaya@gmail.com>
2023-04-08 15:49:50 +0700

    setup redis

---
+++ server/connection/redis_connection.go
@@ -0,0 +1,35 @@
+package connection
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func GetConnectionRedis() (*redis.Client, error) {
+	// url := os.Getenv("REDIS_URL")
+	url := "redis://guest:guest@localhost:6379"
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: url,
+	})
+
+	maxRetries := 10
+	for i := 0; i < maxRetries; i++ {
+		err := rdb.Ping(context.Background()).Err()
+		if err == nil {
+			log.Println("success connect to redis")
+			return rdb, nil
+		}
+
+		log.Println("failed to connect to redis, try again in 1 minute")
+		time.Sleep(1 * time.Minute)
+	}
+
+	log.Println("failed to connect to redis after", maxRetries, "tries")
+	return nil, errors.New("failed connection")
+
+}
--- server/go.mod
+++ server/go.mod
@@ -5,6 +5,8 @@ go 1.19
 require gorm.io/driver/mysql v1.4.7
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -13,6 +15,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/rabbitmq/amqp091-go v1.8.0 // indirect
+	github.com/redis/go-redis/v9 v9.0.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
